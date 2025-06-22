04f5cc7623076cf21f8feb2b8aa51b7a47b2ffab
Anurag <anurag.vishwakrma1408@gmail.com>
2025-06-22 23:04:10 +0530

    [ADD] Two sum

---
+++ list/two_sum.py
@@ -0,0 +1,19 @@
+"""Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target"""
+
+# def twosum(nums, target):
+#     for i in range(0,len(nums)-1):
+#         for j in range(i+1, len(nums)):
+#             if nums[i]+nums[j]==target:
+#                 return [i,j]
+#     return [0,0]
+
+def twosum(nums, target):
+    num_map = {}  # Store value: index
+    for i, num in enumerate(nums,start=0):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+
+print(twosum([1,2,3,4],7))
