7307ff52e3a1023550a43effef19ef36e4d679f5
Anurag <anurag.vishwakrma1408@gmail.com>
2025-07-01 23:05:07 +0530

    [ADD] detect cycle

---
+++ linked-list/detect_cycle.py
@@ -0,0 +1,45 @@
+# Check if the linked list has a loop
+
+
+class LinkNode:
+
+    def __init__(self, val=0, nxt = None):
+        self.val = val
+        self.nxt = nxt
+
+    @staticmethod
+    def is_loop_exist(node):
+        visited = set()
+        current = node
+        while current:
+            if current in visited:
+                print("Loop exists.")
+                return True
+            visited.add(current)
+            current = current.nxt
+        print("Loop does not exist.")
+        return False
+
+
+    @staticmethod
+    def print_list(head):
+        while head:
+            print(head.val, end="-->")
+            head=head.nxt
+        print(None)
+
+
+
+n1=LinkNode(1)
+n2=LinkNode(2)
+n3=LinkNode(3)
+n4=LinkNode(4)
+n1.nxt = n2
+n2.nxt = n3
+n3.nxt = n4
+n4.nxt = n3
+
+# LinkNode.print_list(n1)
+LinkNode.is_loop_exist(n1)
+
+
